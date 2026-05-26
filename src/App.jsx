8e9875968179ef4033906a1085a0a0a9d201046c
adelenurlanovna-jpg <adele.nurlanovna@gmail.com>
2026-05-26 15:26:03 +0300

    Self-host icons, preconnect thum.io, preload LCP, lazy decorative images

--- src/App.jsx
+++ src/App.jsx
@@ -900,7 +900,7 @@ function MarqueeTile({ src }) {
       ) : (
         <img
           src={src}
-          alt=""
+          alt="" loading="lazy"
           loading="lazy"
           className={`w-full h-full block ${isLocal ? 'object-contain' : 'object-cover'}`}
           onError={(e) => {
@@ -999,8 +999,8 @@ function AboutSection() {
         className="absolute top-[4%] left-[1%] sm:left-[2%] md:left-[4%] z-0"
       >
         <img
-          src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/moon_icon.11395d36.png"
-          alt=""
+          src="/assets/moon_icon.11395d36.webp"
+          alt="" loading="lazy"
           className="w-[120px] sm:w-[160px] md:w-[210px] select-none pointer-events-none"
           draggable={false}
         />
@@ -1014,8 +1014,8 @@ function AboutSection() {
         className="absolute top-[4%] right-[1%] sm:right-[2%] md:right-[4%] z-0"
       >
         <img
-          src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/lego_icon-1.703bb594.png"
-          alt=""
+          src="/assets/lego_icon-1.703bb594.webp"
+          alt="" loading="lazy"
           className="w-[120px] sm:w-[160px] md:w-[210px] select-none pointer-events-none"
           draggable={false}
         />
@@ -1029,8 +1029,8 @@ function AboutSection() {
         className="absolute bottom-[8%] left-[3%] sm:left-[6%] md:left-[10%] z-0"
       >
         <img
-          src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/p59_1.4659672e.png"
-          alt=""
+          src="/assets/p59_1.4659672e.webp"
+          alt="" loading="lazy"
           className="w-[100px] sm:w-[140px] md:w-[180px] select-none pointer-events-none"
           draggable={false}
         />
@@ -1044,8 +1044,8 @@ function AboutSection() {
         className="absolute bottom-[8%] right-[3%] sm:right-[6%] md:right-[10%] z-0"
       >
         <img
-          src="https://shrug-person-78902957.figma.site/_components/v2/ebb2b8f25d8e24d5f0a5ca8af4c950de81aa2fd7/Group_134-1.2e04f3ce.png"
-          alt=""
+          src="/assets/Group_134-1.2e04f3ce.webp"
+          alt="" loading="lazy"
           className="w-[130px] sm:w-[170px] md:w-[220px] select-none pointer-events-none"
           draggable={false}
         />
